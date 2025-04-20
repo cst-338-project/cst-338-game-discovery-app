49acf35fdf67d8f82cb9c4aec0bd8118c0b1661c
jjustinn53 <jjustinn53@gmail.com>
2025-04-20 02:23:07 -0700

    Added back button Logout button - no functionality

--- app/src/main/java/com/cst338/lootcrate/LandingPageActivity.java
+++ app/src/main/java/com/cst338/lootcrate/LandingPageActivity.java
@@ -1,6 +1,9 @@
 package com.cst338.lootcrate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.cst338.lootcrate.database.LootCrateRepository;
+import com.cst338.lootcrate.database.entities.User;
+import com.cst338.lootcrate.databinding.ActivityLandingPageBinding;
+
 public class LandingPageActivity extends AppCompatActivity {
 
+    private ActivityLandingPageBinding binding;
+
+    private LootCrateRepository repository;
+    private int loggedInUserId = -1;
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_landing_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        if (user != null) {
+            if (user.isAdmin()) {
+                binding.profileButton.setText("admin");
+            } else {
+                binding.profileButton.setText("profile");
+            }
+        }
+
+        binding.profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ProfilePageActivity.profileIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
         });
+    }
+
+    static Intent landingIntentFactory(Context context) {
+        return new Intent(context, LandingPageActivity.class);
     }
 }
