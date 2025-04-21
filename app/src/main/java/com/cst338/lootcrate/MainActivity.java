515eb3b0d41a89aeed086c8afdc7025809e3240f
AlbertoCsumb <albrodriguez@csumb.edu>
2025-04-20 23:14:00 -0700

    Simple Main activity_main.xml

--- app/src/main/java/com/cst338/lootcrate/MainActivity.java
+++ app/src/main/java/com/cst338/lootcrate/MainActivity.java
@@ -4,18 +4,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+import androidx.room.RoomSQLiteQuery;
 
 import com.cst338.lootcrate.database.LootCrateRepository;
+import com.cst338.lootcrate.database.entities.User;
+import com.cst338.lootcrate.databinding.ActivityLoginBinding;
 
 public class MainActivity extends AppCompatActivity {
     private LootCrateRepository repository;
+    private ActivityLoginBinding binding;
 
+
+    public static Intent mainActivityIntentFactory(Context applicationContext, int id) {
+        Intent intent = new Intent(applicationContext, MainActivity.class);
+        intent.putExtra("userId", id);
+        return intent;
+    }
 
 
     @Override
@@ -34,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MainActivity", "Admin user not found");
             }
         });
+
+
+
 
     }
 }
