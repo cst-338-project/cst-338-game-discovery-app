a2ded7b22b7f67202b2c0af1b6db5cf4787b4be1
Justin Nguyen <160275981+jjustinn53@users.noreply.github.com>
2025-04-21 15:35:28 -0700

    added swipe entity and dao (likes and dislikes)

---
+++ app/src/main/java/com/cst338/lootcrate/database/SwipeDAO.java
@@ -0,0 +1,26 @@
+package com.cst338.lootcrate.database;
+
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.cst338.lootcrate.database.entities.Swipe;
+
+import java.util.List;
+
+public interface SwipeDAO {
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    void insert(Swipe swipe);
+
+    @Query("SELECT * FROM " + LootCrateDatabase.SWIPE_TABLE + " WHERE userId == :userId AND gameId == :gameId LIMIT 1")
+    Swipe getLikeDislikeForUserAndGame(int userId, int gameId);
+
+
+    @Query("SELECT gameId FROM " + LootCrateDatabase.SWIPE_TABLE + " WHERE userId == :userId AND isLiked == 1")
+    List<Integer> getAllLikedGameIdsForUser(int userId);
+
+    @Query("SELECT gameId FROM " + LootCrateDatabase.SWIPE_TABLE + " WHERE userId == :userId AND isLiked == 0")
+    List<Integer> getAllDislikedGameIdsForUser(int userId);
+
+}
---
+++ app/src/main/java/com/cst338/lootcrate/database/entities/Swipe.java
@@ -0,0 +1,79 @@
+package com.cst338.lootcrate.database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.cst338.lootcrate.database.LootCrateDatabase;
+
+import java.util.Objects;
+
+@Entity(tableName = LootCrateDatabase.SWIPE_TABLE)
+public class Swipe {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int userId;
+    private int gameId;
+    private boolean isLiked;
+
+    public Swipe(int gameId, int userId, int id, boolean isLiked) {
+        this.gameId = gameId;
+        this.userId = userId;
+        this.id = id;
+        this.isLiked = isLiked;
+    }
+
+    @Override
+    public String toString() {
+        return "Swipe{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", gameId=" + gameId +
+                ", isLiked=" + isLiked +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Swipe)) return false;
+        Swipe swipe = (Swipe) o;
+        return id == swipe.id && userId == swipe.userId && gameId == swipe.gameId && isLiked == swipe.isLiked;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, gameId, isLiked);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
+    }
+}
