2d38b4965968b41021c0187e073ee7eea8362bce
Ethan Ayaay <ayaayethan@gmail.com>
2025-04-26 18:45:52 -0700

    created Rawg interface and APIClient class

---
+++ app/src/main/java/com/cst338/lootcrate/retroFit/RAWGApiService.java
@@ -0,0 +1,14 @@
+package com.cst338.lootcrate.retroFit;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface RAWGApiService {
+    @GET("games")
+    Call<GamesResponse> getGames(
+            @Query("key") String apiKey,
+            @Query("page") int page,
+            @Query("page_size") int pageSize
+    );
+}
