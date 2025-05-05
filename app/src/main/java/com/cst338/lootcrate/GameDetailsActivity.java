4b2cd6eb27734bbe7053bf7acd2c0c804bf41c6f
Brandon Algarra <balgarra@csumb.edu>
2025-05-05 00:41:32 -0700

    Implemented viewpager to show multiple images on details screen with swiping

--- app/src/main/java/com/cst338/lootcrate/GameDetailsActivity.java
+++ app/src/main/java/com/cst338/lootcrate/GameDetailsActivity.java
@@ -7,12 +7,17 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.bumptech.glide.Glide;
 import com.cst338.lootcrate.database.GameDAO;
 import com.cst338.lootcrate.database.LootCrateRepository;
 import com.cst338.lootcrate.database.entities.Game;
 import com.cst338.lootcrate.databinding.ActivityGameDetailsBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GameDetailsActivity extends AppCompatActivity {
 
@@ -23,6 +28,7 @@ public class GameDetailsActivity extends AppCompatActivity {
     private int userId = 1;
     private Game game;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +38,20 @@ public class GameDetailsActivity extends AppCompatActivity {
         gameId = getIntent().getIntExtra("GameId", gameId);
         game = repository.getGameById(gameId);
 
+        ViewPager2 viewPager2 = findViewById(R.id.gameDetailsViewPager);
+
+        List<String> imageUrls = Arrays.asList(
+                game.getScreenshot1(),
+                game.getScreenshot2(),
+                game.getScreenshot3()
+        );
+
+        ImageAdapter adapter = new ImageAdapter(this, imageUrls);
+        viewPager2.setAdapter(adapter);
+
         Log.d("GAME SCREENSHOTS", game.getScreenshot1() + game.getScreenshot2() + game.getScreenshot3());
 
        setInfo();
-       //TODO: Use userId for back button
        binding.gameDetailsBackButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
@@ -48,9 +64,9 @@ public class GameDetailsActivity extends AppCompatActivity {
         binding.gameDetailsTitleTextView.setText(game.getTitle());
         binding.gameDetailsGenreTextView.setText(game.getGenre());
         binding.gameDetailsDescriptionTextView.setText(game.getDescription());
-        Glide.with(this)
-                .load(game.getImageUrl())
-                .into(binding.gameDetailsCoverImageView);
+//        Glide.with(this)
+//                .load(game.getImageUrl())
+//                .into(binding.gameDetailsCoverImageView);
         binding.gameDetailsMetacriticScoreTextView.setText(String.valueOf("Metacritic: " + game.getMetacritic()));
         binding.gameDetailsLinksTextView.setText(game.getWebsite());
 
---
+++ app/src/main/java/com/cst338/lootcrate/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.cst338.lootcrate;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private List<String> imageUrls;
+    private Context context;
+
+    public ImageAdapter(Context context, List<String> imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ImageView imageView = new ImageView(context);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return new ImageViewHolder(imageView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        Glide.with(context)
+                .load(imageUrls.get(position))
+            // Optional placeholder image
+                    // Optional error image
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ImageViewHolder(@NonNull ImageView itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView;
+        }
+    }
+}
